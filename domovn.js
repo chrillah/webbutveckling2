2f45d36b1b5d3f6c2de2b360778b3e93cf809faf
chrillah <ch.ronnberg@gmail.com>
2023-07-19 21:14:08 +0200

    fix

--- domovn.js
+++ domovn.js
@@ -9,44 +9,44 @@
     // sätt klassnamnet bodyCopy på dessa
 
     // Här selekterar vi tag i alla p-taggar/ element och förvarar det i en lista med noder
-    var listOfParagraphs = document.getElementsByTagName('p');
+    var paragraphList = document.getElementsByTagName('p');
 
     // Här sätter vi klassnamnet "bodyCopy" till alla p-element med en for lopp
     // loppen startar på 0, vilket är det första indexplatsen i listan av noder
     // och loopar tills den når sista indexplatsen
     for (var i = 0; i < listOfParagraphs.length; i++) {
-        listOfParagraphs[i].className = 'bodyCopy';
+        paragraphList[i].className = 'bodyCopy';
     }
 
     // 2) Selektorer och childnodes för klassmanipulation
     // Manipulera p-taggen i footer och sätt klassnamnet "footerp"
 
     // kollade igenom alla p-taggar genom att ändra indexplatsen inom hakparanterserna
-    listOfParagraphs[3].className = 'footerp';
+    paragraphList[3].className = 'footerp';
 
     // 3) Class name selektorer, loopar och DOM manipulation
     // Skapa länkar utifrån navigations-listans <li> och dess text
 
     // Selekterar rätt list-element genom att använda mig utav getElementByClassName
     // får en lista med li-element
-    var navigationListItems = document.getElementsByClassName('navigation');
+    var navigationList = document.getElementsByClassName('navigation');
 
     // loopar igenom listan
-    for (i = 0; i < navigationListItems.length; i++) {
+    for (i = 0; i < navigationList.length; i++) {
         // Skapar länk-taggen a med createElement
-        var linkTag = document.createElement('a');
+        var aElement = document.createElement('a');
 
         // Lägger till attributen med rätt länk från li-elementens innehåll
-        linkTag.setAttribute('href', '#' + navigationListItems[i].innerHTML);
+        aElement.setAttribute('href', '#' + navigationList[i].innerHTML);
 
         // Får rätt innehållsvärde
-        linkTag.innerHTML = navigationListItems[i].innerHTML;
+        aElement.innerHTML = navigationList[i].innerHTML;
 
         // Tar bort li-taggens inehåll
-        navigationListItems[i].innerHTML = '';
+        navigationList[i].innerHTML = '';
 
         // Lägger till a-taggen till föräldern i det här fallet, li-taggen
-        navigationListItems[i].appendChild(linkTag);
+        navigationList[i].appendChild(aElement);
     }
 
     // 4) Id selektorer och attribut
@@ -68,15 +68,18 @@
     // Väljer rätt ID och förvarar värdet i en variabel
     var list1 = document.getElementById('lista1');
 
-    // Loopar igenom list1-varibelns children, vilket är en lista med alla li-taggar
-    // som finns i "#lista1"
-    for(i = 0; i < list1.children.length; i++){
+    // list1-varibelns children, vilket är en lista med alla li-taggar
+    var list1Children = list1.children;
 
-        // Kollar om li-taggens plats i indexet är ojämnt med hjälp av modulu %
+    console.log(list1Children.length)
+    // Loopar igenom
+    for(i = 0; i <= list1Children.length; i++){
+        // Kollar om li-taggens plats i indexet är ojämnt med hjälp av modulus %
         // modulus kollar om det blir rest över när summan går jämnt ut
-        // Här kollar om det inte blir jämnt ut, det vill säga, om det blir rest
-        if(i % 2 !== 0)
+        // Här kollar om det blir jämnt ut, det vill säga, om det blir rest
+        if(i % 2 === 0){
         // och om det stämmer så får li-taggen i en ojämn indexplats en röd textfärg
-        list1.children[i].style.color = "red";
+        list1Children[i].style.color = "red";
+        }
     }
 })();
